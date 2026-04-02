ec8445ce37571d9b26f06c944068103a2c252e47
Kishika Jain <kishikajain2206@gmail.com>
2026-04-02 18:00:35 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+// ✅ CORS FIX (important)
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
